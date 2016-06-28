165c10587fc7574635855fc2baf5d3bfce18cd33
Longri <www.longri.de@googlemail.com>
2016-06-28 12:49:59 +0200

    create LibGdx project with gdx-setup 1.9.3

---
+++ android/src/de/longri/cachebox3/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.longri.cachebox3;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.longri.cachebox3.CacheboxMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CacheboxMain(), config);
+	}
+}
---
+++ desktop/src/de/longri/cachebox3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.longri.cachebox3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.longri.cachebox3.CacheboxMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CacheboxMain(), config);
+	}
+}
