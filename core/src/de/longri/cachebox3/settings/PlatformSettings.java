f5b95cf62ce450474cfd4f803fdd35cb248ef775
Longri <www.longri.de@googlemail.com>
2016-08-03 15:32:10 +0200

    implement PlatformSettings

--- core/src/de/longri/cachebox3/settings/PlatformSettings.java
+++ core/src/de/longri/cachebox3/settings/PlatformSettings.java
@@ -1,36 +1,56 @@
 package de.longri.cachebox3.settings;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class PlatformSettings {
-	// ------ setPlatformSetting ------
-	public interface IPlatformSettings {
-		public SettingBase<?> Read(SettingBase<?> setting);
 
-		public void Write(SettingBase<?> setting);
 
-	}
+    private static Preferences prefs;
 
-	public static IPlatformSettings platformSettingsListener;
 
-	public static void setPlatformSettings(IPlatformSettings listener) {
-		platformSettingsListener = listener;
-	}
+    private static void checkPrefs() {
+        if (prefs == null) {
+            prefs = Gdx.app.getPreferences("CacheboxPreferences");
+        }
+    }
 
-	public static SettingBase<?> ReadSetting(SettingBase<?> setting) {
-		if (platformSettingsListener != null)
-			setting = platformSettingsListener.Read(setting);
-		return setting;
-	}
 
-	public static <T> void WriteSetting(SettingBase<T> setting) {
-		if (platformSettingsListener != null)
-			platformSettingsListener.Write(setting);
-	}
+    public static SettingBase<?> ReadSetting(SettingBase<?> setting) {
+        checkPrefs();
+        if (setting instanceof SettingString) {
+            String value = prefs.getString(setting.getName(), "");
+            ((SettingString) setting).setValue(value);
+        } else if (setting instanceof SettingBool) {
+            boolean value = prefs.getBoolean(setting.getName(), ((SettingBool) setting).getDefaultValue());
+            ((SettingBool) setting).setValue(value);
+        } else if (setting instanceof SettingInt) {
+            int value = prefs.getInteger(setting.getName(), ((SettingInt) setting).getDefaultValue());
+            ((SettingInt) setting).setValue(value);
+        }
+        setting.clearDirty();
+        return setting;
+    }
 
-	/**
-	 * @return True, if platform settings are set
-	 */
-	public static boolean canUsePlatformSettings() {
-		return (platformSettingsListener != null);
-	}
+    public static <T> void WriteSetting(SettingBase<T> setting) {
+        checkPrefs();
+
+        if (setting instanceof SettingBool) {
+            prefs.putBoolean(setting.getName(), ((SettingBool) setting).getValue());
+        } else if (setting instanceof SettingString) {
+            prefs.putString(setting.getName(), ((SettingString) setting).getValue());
+        } else if (setting instanceof SettingInt) {
+            prefs.putInteger(setting.getName(), ((SettingInt) setting).getValue());
+        }
+        // Commit the edits!
+        prefs.flush();
+    }
+
+    /**
+     * @return True, if platform settings are set
+     */
+    public static boolean canUsePlatformSettings() {
+        return true;
+    }
 
 }
