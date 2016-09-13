1e9e6a53f1acf4aaeb18cf268e02ed72bb4f65b2
Longri <www.longri.de@googlemail.com>
2016-09-13 12:40:48 +0200

    set MapPosition with incoming new GPS position

--- core/src/de/longri/cachebox3/gui/views/MapView.java
+++ core/src/de/longri/cachebox3/gui/views/MapView.java
@@ -20,15 +20,22 @@ import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import de.longri.cachebox3.CacheboxMain;
 import de.longri.cachebox3.gui.stages.StageManager;
+import de.longri.cachebox3.locator.Location;
+import de.longri.cachebox3.locator.Locator;
+import de.longri.cachebox3.locator.events.PositionChangedEvent;
+import de.longri.cachebox3.locator.events.PositionChangedEventList;
+import org.oscim.core.MapPosition;
 import org.oscim.gdx.LayerHandler;
 import org.oscim.gdx.MotionHandler;
+
+import static de.longri.cachebox3.CacheboxMain.mMap;
 
 /**
  * The MapView has transparent background. The Map render runs at CacheboxMain.
  * This View has only the controls for the Map!
  * Created by Longri on 24.07.16.
  */
-public class MapView extends AbstractView {
+public class MapView extends AbstractView implements PositionChangedEvent {
 
     InputMultiplexer mapInputHandler;
 
@@ -46,25 +53,55 @@ public class MapView extends AbstractView {
     public void onShow() {
         CacheboxMain.drawMap = true;
         // map input handler
-        GestureDetector gestureDetectore = new GestureDetector(new LayerHandler(CacheboxMain.mMap));
-        MotionHandler motionHandler = new MotionHandler(CacheboxMain.mMap);
-        MapInputHandler inputHandler = new MapInputHandler(CacheboxMain.mMap);
+        GestureDetector gestureDetectore = new GestureDetector(new LayerHandler(mMap));
+        MotionHandler motionHandler = new MotionHandler(mMap);
+        MapInputHandler inputHandler = new MapInputHandler(mMap);
         mapInputHandler = new InputMultiplexer();
         mapInputHandler.addProcessor(motionHandler);
         mapInputHandler.addProcessor(gestureDetectore);
         mapInputHandler.addProcessor(inputHandler);
         StageManager.addMapMultiplexer(mapInputHandler);
+        PositionChangedEventList.Add(this);
     }
 
     @Override
     public void onHide() {
         CacheboxMain.drawMap = false;
         StageManager.removeMapMultiplexer(mapInputHandler);
+        PositionChangedEventList.Remove(this);
     }
 
 
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public void PositionChanged() {
+        MapPosition curentMapPosition = mMap.getMapPosition();
+        Location curentLocation = Locator.getLocation();
+        curentMapPosition.setPosition(curentLocation.latitude, curentLocation.longitude);
+        mMap.setMapPosition(curentMapPosition);
+    }
+
+    @Override
+    public void OrientationChanged() {
+
+    }
+
+    @Override
+    public void SpeedChanged() {
+
+    }
+
+    @Override
+    public String getReceiverName() {
+        return "MapView";
+    }
+
+    @Override
+    public Priority getPriority() {
+        return Priority.High;
     }
 }
