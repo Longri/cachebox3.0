ab325bd67882713990dccc1ceec168be51075cac
Longri <www.longri.de@googlemail.com>
2016-10-16 18:17:04 +0200

    ZoomButtons fade out

--- core/src/de/longri/cachebox3/gui/widgets/ZoomButton.java
+++ core/src/de/longri/cachebox3/gui/widgets/ZoomButton.java
@@ -16,7 +16,10 @@
 package de.longri.cachebox3.gui.widgets;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.kotcrab.vis.ui.VisUI;
 
@@ -25,9 +28,26 @@ import com.kotcrab.vis.ui.VisUI;
  */
 public class ZoomButton extends Group {
 
+    private final float TIME_TO_FADE_OUT = 5;
+    private float actTime;
+    private boolean fadeOutRuns = false;
+
     private final ZoomButtonStyle style;
     private final Button plus;
     private final Button minus;
+
+
+    private ClickListener clickListener = new ClickListener() {
+        @Override
+        public void clicked(InputEvent event, float x, float y) {
+            if (fadeOutRuns) {
+                ZoomButton.this.clearActions();
+                actTime = 0;
+                fadeOutRuns = false;
+            }
+            ZoomButton.this.addAction(Actions.alpha(style.alphaOn));
+        }
+    };
 
     public ZoomButton() {
         style = VisUI.getSkin().get("default", ZoomButtonStyle.class);
@@ -35,12 +55,18 @@ public class ZoomButton extends Group {
         Button.ButtonStyle btnStylePlus = new Button.ButtonStyle();
         btnStylePlus.up = style.plus;
         btnStylePlus.down = style.plusDown;
+        btnStylePlus.disabled = style.plusDisabled;
         plus = new Button(btnStylePlus);
 
         Button.ButtonStyle btnStyleMinus = new Button.ButtonStyle();
         btnStyleMinus.up = style.minus;
         btnStyleMinus.down = style.minusDown;
+        btnStyleMinus.disabled = style.minusDisabled;
         minus = new Button(btnStyleMinus);
+
+
+        plus.addListener(clickListener);
+        minus.addListener(clickListener);
 
         this.addActor(plus);
         this.addActor(minus);
@@ -57,9 +83,28 @@ public class ZoomButton extends Group {
         this.plus.setBounds(widthMinus, 0, widthPlus, height);
     }
 
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        actTime += delta;
+
+        if (!fadeOutRuns && actTime > TIME_TO_FADE_OUT) {
+            this.addAction(Actions.alpha(style.alphaOff, 2f));
+            this.fadeOutRuns = true;
+        }
+    }
+
 
     public static class ZoomButtonStyle {
-        Drawable plus, minus, plusDown, minusDown;
+        Drawable plus, minus, plusDown, minusDown, plusDisabled, minusDisabled;
         float alphaOff = 0.2f, alphaOn = 1.0f;
+    }
+
+    public void setDisabledPlus(boolean disabeld) {
+        this.plus.setDisabled(disabeld);
+    }
+
+    public void setDisabledMinus(boolean disabeld) {
+        this.minus.setDisabled(disabeld);
     }
 }
