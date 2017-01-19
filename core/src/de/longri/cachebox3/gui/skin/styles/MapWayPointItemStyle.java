5fb180c4bccbd195e5ca9e4e15f9e540f98d3e80
Longri <www.longri.de@googlemail.com>
2017-01-19 09:56:41 +0100

    [core] add description for MapWayPointItemStyle

--- core/src/de/longri/cachebox3/gui/skin/styles/MapWayPointItemStyle.java
+++ core/src/de/longri/cachebox3/gui/skin/styles/MapWayPointItemStyle.java
@@ -1,8 +1,28 @@
+/*
+ * Copyright (C) 2017 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.longri.cachebox3.gui.skin.styles;
 
 import org.oscim.backend.canvas.Bitmap;
 
 /**
+ * Style for map icons.
+ * [small] => zoom level < 13
+ * [middle] => zoom level 13 - 14
+ * [large] => zoom level > 14
+ * <p>
  * Created by Longri on 19.01.2017.
  */
 public class MapWayPointItemStyle {
