61994d1398f015020e7311b3d529f58d05c329e3
Longri <develop@longri.de>
2017-06-25 10:12:51 +0200

    [Core] fix close #133 Attribute positive, negative wrong

--- core/src/de/longri/cachebox3/types/CacheDetail.java
+++ core/src/de/longri/cachebox3/types/CacheDetail.java
@@ -16,7 +16,6 @@
 package de.longri.cachebox3.types;
 
 
-
 import com.badlogic.gdx.sql.SQLiteGdxDatabaseCursor;
 import de.longri.cachebox3.sqlite.Database;
 import de.longri.cachebox3.utils.lists.CB_List;
@@ -26,279 +25,267 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class CacheDetail implements Serializable {
-	private static final long serialVersionUID = 2088367633865443637L;
-	// final static Logger log = LoggerFactory.getLogger(CacheDetail.class);
+    private static final long serialVersionUID = 2088367633865443637L;
+    // final static Logger log = LoggerFactory.getLogger(CacheDetail.class);
 
 	/*
-	 * Public Member
+     * Public Member
 	 */
 
-	/**
-	 * Erschaffer des Caches
-	 */
-	public String PlacedBy = "";
+    /**
+     * Erschaffer des Caches
+     */
+    public String PlacedBy = "";
 
-	/**
-	 * Datum, an dem der Cache versteckt wurde
-	 */
-	public Date DateHidden;
-	/**
-	 * apiState 0: Cache wurde nicht per Api hinzugefuegt
-	 *           1: Cache wurde per GC Api hinzugefuegt und ist noch nicht komplett geladen (IsLite = true) 
-	 *           2: Cache wurde per GC Api hinzugefuegt und ist komplett geladen (IsLite = false)
-	 */
-	public byte apiState;
+    /**
+     * Datum, an dem der Cache versteckt wurde
+     */
+    public Date DateHidden;
+    /**
+     * apiState 0: Cache wurde nicht per Api hinzugefuegt
+     * 1: Cache wurde per GC Api hinzugefuegt und ist noch nicht komplett geladen (IsLite = true)
+     * 2: Cache wurde per GC Api hinzugefuegt und ist komplett geladen (IsLite = false)
+     */
+    public byte apiState;
 
-	/**
-	 * for Replication
-	 */
-	public int noteCheckSum = 0;
-	public String tmpNote = null; // nur fuer den RPC-Import
+    /**
+     * for Replication
+     */
+    public int noteCheckSum = 0;
+    public String tmpNote = null; // nur fuer den RPC-Import
 
-	/**
-	 * for Replication
-	 */
-	public int solverCheckSum = 0;
-	public String tmpSolver = null; // nur fuer den RPC-Import
+    /**
+     * for Replication
+     */
+    public int solverCheckSum = 0;
+    public String tmpSolver = null; // nur fuer den RPC-Import
 
-	/**
-	 * Name der Tour, wenn die GPX-Datei aus GCTour importiert wurde
-	 */
-	public String TourName = "";
+    /**
+     * Name der Tour, wenn die GPX-Datei aus GCTour importiert wurde
+     */
+    public String TourName = "";
 
-	/**
-	 * Name der GPX-Datei aus der importiert wurde
-	 */
-	public long GPXFilename_ID = 0;
+    /**
+     * Name der GPX-Datei aus der importiert wurde
+     */
+    public long GPXFilename_ID = 0;
 
-	/**
-	 * URL des Caches
-	 */
-	public String Url = "";
+    /**
+     * URL des Caches
+     */
+    public String Url = "";
 
-	/**
-	 * Country des Caches
-	 */
-	public String Country = "";
+    /**
+     * Country des Caches
+     */
+    public String Country = "";
 
-	/**
-	 * MapMode des Caches
-	 */
-	public String State = "";
+    /**
+     * MapMode des Caches
+     */
+    public String State = "";
 
-	/**
-	 * Positive Attribute des Caches
-	 */
-	private DLong attributesPositive = new DLong(0, 0);
+    /**
+     * Positive Attribute des Caches
+     */
+    private DLong attributesPositive = new DLong(0, 0);
 
-	/**
-	 * Negative Attribute des Caches
-	 */
-	private DLong attributesNegative = new DLong(0, 0);
+    /**
+     * Negative Attribute des Caches
+     */
+    private DLong attributesNegative = new DLong(0, 0);
 
-	/**
-	 * Hinweis fuer diesen Cache
-	 */
-	private String hint = "";
+    /**
+     * Hinweis fuer diesen Cache
+     */
+    private String hint = "";
 
-	/**
-	 * Liste der Spoiler Ressourcen
-	 */
-	private CB_List<ImageEntry> spoilerRessources = null;
+    /**
+     * Liste der Spoiler Ressourcen
+     */
+    private CB_List<ImageEntry> spoilerRessources = null;
 
-	/**
-	 * Kurz Beschreibung des Caches
-	 */
-	public String shortDescription;
+    /**
+     * Kurz Beschreibung des Caches
+     */
+    public String shortDescription;
 
-	/**
-	 * Ausfuehrliche Beschreibung des Caches Nur fuer Import Zwecke. Ist normalerweise leer, da die Description bei aus Speicherplatz
-	 * Gruenden bei Bedarf aus der DB geladen wird
-	 */
-	public String longDescription;
+    /**
+     * Ausfuehrliche Beschreibung des Caches Nur fuer Import Zwecke. Ist normalerweise leer, da die Description bei aus Speicherplatz
+     * Gruenden bei Bedarf aus der DB geladen wird
+     */
+    public String longDescription;
 
 	/*
 	 * Constructors
 	 */
 
-	/**
-	 * Constructor
-	 */
-	public CacheDetail() {
-		this.DateHidden = new Date();
-		AttributeList = null;
+    /**
+     * Constructor
+     */
+    public CacheDetail() {
+        this.DateHidden = new Date();
+    }
 
-	}
+    public void dispose() {
+        // clear all Lists
+        
+        if (spoilerRessources != null) {
+            for (int i = 0, n = spoilerRessources.size; i < n; i++) {
+                ImageEntry entry = spoilerRessources.get(i);
+                entry.dispose();
+            }
+            spoilerRessources.clear();
+            spoilerRessources = null;
+        }
 
-	public void dispose() {
-		// clear all Lists
-		if (AttributeList != null) {
-			AttributeList.clear();
-			AttributeList = null;
-		}
+        // if (waypoints != null)
+        // {
+        // for (int i = 0, n = waypoints.size(); i < n; i++)
+        // {
+        // Waypoint entry = waypoints.get(i);
+        // entry.dispose();
+        // }
+        //
+        // waypoints.clear();
+        // }
 
-		if (spoilerRessources != null) {
-			for (int i = 0, n = spoilerRessources.size; i < n; i++) {
-				ImageEntry entry = spoilerRessources.get(i);
-				entry.dispose();
-			}
-			spoilerRessources.clear();
-			spoilerRessources = null;
-		}
+        tmpNote = null;
+        tmpSolver = null;
+        TourName = null;
+        PlacedBy = null;
+        // setOwner(null);
+        DateHidden = null;
+        Url = null;
+        Country = null;
+        State = null;
+        // setHint(null);
+        shortDescription = null;
+        longDescription = null;
 
-		// if (waypoints != null)
-		// {
-		// for (int i = 0, n = waypoints.size(); i < n; i++)
-		// {
-		// Waypoint entry = waypoints.get(i);
-		// entry.dispose();
-		// }
-		//
-		// waypoints.clear();
-		// }
+    }
 
-		tmpNote = null;
-		tmpSolver = null;
-		TourName = null;
-		PlacedBy = null;
-		// setOwner(null);
-		DateHidden = null;
-		Url = null;
-		Country = null;
-		State = null;
-		// setHint(null);
-		shortDescription = null;
-		longDescription = null;
+    public boolean isAttributePositiveSet(Attributes attribute) {
+        return attributesPositive.BitAndBiggerNull(Attributes.GetAttributeDlong(attribute));
+        // return (attributesPositive & Attributes.GetAttributeDlong(attribute))
+        // > 0;
+    }
 
-	}
+    public boolean isAttributeNegativeSet(Attributes attribute) {
+        return attributesNegative.BitAndBiggerNull(Attributes.GetAttributeDlong(attribute));
+        // return (attributesNegative & Attributes.GetAttributeDlong(attribute))
+        // > 0;
+    }
 
-	public boolean isAttributePositiveSet(Attributes attribute) {
-		return attributesPositive.BitAndBiggerNull(Attributes.GetAttributeDlong(attribute));
-		// return (attributesPositive & Attributes.GetAttributeDlong(attribute))
-		// > 0;
-	}
+    public void addAttributeNegative(Attributes attribute) {
+        if (attributesNegative == null)
+            attributesNegative = new DLong(0, 0);
+        attributesNegative.BitOr(Attributes.GetAttributeDlong(attribute));
+    }
 
-	public boolean isAttributeNegativeSet(Attributes attribute) {
-		return attributesNegative.BitAndBiggerNull(Attributes.GetAttributeDlong(attribute));
-		// return (attributesNegative & Attributes.GetAttributeDlong(attribute))
-		// > 0;
-	}
+    public void addAttributePositive(Attributes attribute) {
+        if (attributesPositive == null)
+            attributesPositive = new DLong(0, 0);
+        attributesPositive.BitOr(Attributes.GetAttributeDlong(attribute));
+    }
 
-	public void addAttributeNegative(Attributes attribute) {
-		if (attributesNegative == null)
-			attributesNegative = new DLong(0, 0);
-		attributesNegative.BitOr(Attributes.GetAttributeDlong(attribute));
-	}
+    public void setAttributesPositive(DLong i) {
+        attributesPositive = i;
+    }
 
-	public void addAttributePositive(Attributes attribute) {
-		if (attributesPositive == null)
-			attributesPositive = new DLong(0, 0);
-		attributesPositive.BitOr(Attributes.GetAttributeDlong(attribute));
-	}
+    public void setAttributesNegative(DLong i) {
+        attributesNegative = i;
+    }
 
-	public void setAttributesPositive(DLong i) {
-		attributesPositive = i;
-	}
+    public DLong getAttributesNegative(long Id) {
+        if (this.attributesNegative == null) {
+            SQLiteGdxDatabaseCursor c = Database.Data.rawQuery("select AttributesNegative,AttributesNegativeHigh from Caches where Id=?", new String[]{String.valueOf(Id)});
+            c.moveToFirst();
+            while (!c.isAfterLast()) {
+                if (!c.isNull(0))
+                    this.attributesNegative = new DLong(c.getLong(1), c.getLong(0));
+                else
+                    this.attributesNegative = new DLong(0, 0);
+                break;
+            }
+            ;
+            c.close();
+        }
+        return this.attributesNegative;
+    }
 
-	public void setAttributesNegative(DLong i) {
-		attributesNegative = i;
-	}
+    public DLong getAttributesPositive(long Id) {
+        if (this.attributesPositive == null) {
+            SQLiteGdxDatabaseCursor c = Database.Data.rawQuery("select AttributesPositive,AttributesPositiveHigh from Caches where Id=?", new String[]{String.valueOf(Id)});
+            c.moveToFirst();
+            while (!c.isAfterLast()) {
+                if (!c.isNull(0))
+                    this.attributesPositive = new DLong(c.getLong(1), c.getLong(0));
+                else
+                    this.attributesPositive = new DLong(0, 0);
+                break;
+            }
+            ;
+            c.close();
+        }
+        return this.attributesPositive;
+    }
 
-	public DLong getAttributesNegative(long Id) {
-		if (this.attributesNegative == null) {
-            SQLiteGdxDatabaseCursor c = Database.Data.rawQuery("select AttributesNegative,AttributesNegativeHigh from Caches where Id=?", new String[] { String.valueOf(Id) });
-			c.moveToFirst();
-			while (!c.isAfterLast()) {
-				if (!c.isNull(0))
-					this.attributesNegative = new DLong(c.getLong(1), c.getLong(0));
-				else
-					this.attributesNegative = new DLong(0, 0);
-				break;
-			}
-			;
-			c.close();
-		}
-		return this.attributesNegative;
-	}
+    public ArrayList<Attributes> getAttributes(long Id) {
+        return Attributes.getAttributes(this.getAttributesPositive(Id), this.getAttributesNegative(Id));
+    }
 
-	public DLong getAttributesPositive(long Id) {
-		if (this.attributesPositive == null) {
-			SQLiteGdxDatabaseCursor c = Database.Data.rawQuery("select AttributesPositive,AttributesPositiveHigh from Caches where Id=?", new String[] { String.valueOf(Id) });
-			c.moveToFirst();
-			while (!c.isAfterLast()) {
-				if (!c.isNull(0))
-					this.attributesPositive = new DLong(c.getLong(1), c.getLong(0));
-				else
-					this.attributesPositive = new DLong(0, 0);
-				break;
-			}
-			;
-			c.close();
-		}
-		return this.attributesPositive;
-	}
+    public String getHint() {
+        return hint;
+    }
 
-	private ArrayList<Attributes> AttributeList = null;
+    public void setHint(String hint2) {
+        this.hint = hint2;
 
-	public ArrayList<Attributes> getAttributes(long Id) {
-		if (AttributeList == null) {
-			AttributeList = Attributes.getAttributes(this.getAttributesPositive(Id), this.getAttributesNegative(Id));
-		}
+    }
 
-		return AttributeList;
-	}
+    /**
+     * Returns a List of Spoiler Ressources
+     *
+     * @return ArrayList of String
+     */
+    public CB_List<ImageEntry> getSpoilerRessources(Cache cache) {
+        if (spoilerRessources == null) {
+            //loadSpoilerRessources(cache);
+        }
 
-	public String getHint() {
-		return hint;
-	}
+        return spoilerRessources;
+    }
 
-	public void setHint(String hint2) {
-		this.hint = hint2;
+    /**
+     * Set a List of Spoiler Ressources
+     *
+     * @param value ArrayList of String
+     */
+    public void setSpoilerRessources(CB_List<ImageEntry> value) {
+        spoilerRessources = value;
+    }
 
-	}
-
-	/**
-	 * Returns a List of Spoiler Ressources
-	 *
-	 * @return ArrayList of String
-	 */
-	public CB_List<ImageEntry> getSpoilerRessources(Cache cache) {
-		if (spoilerRessources == null) {
-			//loadSpoilerRessources(cache);
-		}
-
-		return spoilerRessources;
-	}
-
-	/**
-	 * Set a List of Spoiler Ressources
-	 *
-	 * @param value ArrayList of String
-	 */
-	public void setSpoilerRessources(CB_List<ImageEntry> value) {
-		spoilerRessources = value;
-	}
-
-	/**
-	 * Returns true has the Cache Spoilers else returns false
-	 *
-	 * @return Boolean
-	 */
-	public boolean hasSpoiler(Cache cache) {
-		try {
-			if (spoilerRessources == null)
-return false;
+    /**
+     * Returns true has the Cache Spoilers else returns false
+     *
+     * @return Boolean
+     */
+    public boolean hasSpoiler(Cache cache) {
+        try {
+            if (spoilerRessources == null)
+                return false;
 
 //				loadSpoilerRessources(cache);
-			return spoilerRessources.size > 0;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+            return spoilerRessources.size > 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	public void SpoilerForceReEvaluate(Cache cache) {
-		spoilerRessources = null;
-	}
+    public void SpoilerForceReEvaluate(Cache cache) {
+        spoilerRessources = null;
+    }
 
 //	public void loadSpoilerRessources(Cache cache) {
 //
@@ -405,20 +392,20 @@ return false;
 //		}
 //	}
 
-	public void setLongDescription(String value) {
-		longDescription = value;
-	}
+    public void setLongDescription(String value) {
+        longDescription = value;
+    }
 
-	public String getLongDescription() {
-		return longDescription;
-	}
+    public String getLongDescription() {
+        return longDescription;
+    }
 
-	public void setShortDescription(String value) {
-		shortDescription = value;
-	}
+    public void setShortDescription(String value) {
+        shortDescription = value;
+    }
 
-	public String getShortDescription() {
-		return shortDescription;
-	}
+    public String getShortDescription() {
+        return shortDescription;
+    }
 
 }
