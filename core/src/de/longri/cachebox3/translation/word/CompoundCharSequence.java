94f2d6466ec54c68e1c3e596026dae8b3e34552c
Longri <develop@longri.de>
2017-10-28 09:15:31 +0200

    [Core] add CompoundCharSequence

---
+++ core/src/de/longri/cachebox3/translation/word/CompoundCharSequence.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2017 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.translation.word;
+
+import com.badlogic.gdx.utils.Array;
+
+
+/**
+ * Created by Longri on 28.10.2017.
+ */
+public class CompoundCharSequence implements CharSequence {
+
+
+    private final Array<CharSequence> items = new Array<>();
+
+    public CompoundCharSequence(CharSequence... sequences) {
+        items.addAll(sequences);
+    }
+
+    public void add(CharSequence... sequences) {
+        items.addAll(sequences);
+    }
+
+
+    @Override
+    public int length() {
+        int length = 0;
+        int n = items.size;
+        while (n-- > 0) {
+            length += items.get(n).length();
+        }
+        return length;
+    }
+
+    @Override
+    public char charAt(int index) {
+        int actIdx = 0;
+        int idx = index;
+
+        while (idx >= items.get(actIdx).length()) {
+            idx -= items.get(actIdx++).length();
+            if (actIdx > items.size) {
+                throw new RuntimeException("Index [" + index + "] out of range: " + length());
+            }
+        }
+        return items.get(actIdx).charAt(idx);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0, n = items.size; i < n; i++) {
+            sb.append(items.get(i).toString());
+        }
+        return sb.toString();
+    }
+}
---
+++ core/tests/de/longri/cachebox3/translation/word/CompoundCharSequenceTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2017 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.translation.word;
+
+import com.badlogic.gdx.utils.CharArray;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Longri on 28.10.2017.
+ */
+class CompoundCharSequenceTest {
+
+    @Test
+    void add() {
+
+        String t1 = "Test";
+        StringBuilder sb = new StringBuilder();
+        sb.append("String");
+
+        MutableString mutable1 = new MutableString(new CharArray(), "Compound");
+
+        String shouldSequence = "CompoundTestString";
+
+        CompoundCharSequence sequence = new CompoundCharSequence();
+        sequence.add(mutable1, t1, sb);
+
+        assertThat("Length must equals  '" + shouldSequence.length() + "' : '" + sequence.length() + "'", shouldSequence.length() == sequence.length());
+        assertThat("Sequence must equals  '" + shouldSequence + "' : '" + sequence + "'", equals(shouldSequence, sequence));
+        assertThat("toString() must equals  '" + shouldSequence + "' : '" + sequence + "'", shouldSequence.equals(sequence.toString()));
+
+
+
+    }
+
+    //##################################################################
+    //# Helper
+    //##################################################################
+
+    private boolean equals(CharSequence s1, CharSequence s2) {
+        if (s1.length() != s2.length()) return false;
+        int n = s1.length();
+        while (n-- > 0) {
+            if (s1.charAt(n) != s2.charAt(n)) return false;
+        }
+        return true;
+    }
+
+}
