7afd5508bc8ad8eb3ae59a0412d55bf8a84fe295
Longri <develop@longri.de>
2017-11-08 20:30:03 +0100

    [JUnit] add SkinFont-Test

---
+++ core/tests/com/badlogic/gdx/graphics/g2d/freetype/SkinFontTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.badlogic.gdx.graphics.g2d.freetype;
+
+import de.longri.cachebox3.sqlite.DatabaseVersions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Longri on 08.11.17.
+ */
+class SkinFontTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void calculateFontPageSize() {
+
+        assertThat("TODO implement test for page size calculation", false);
+
+    }
+
+}
