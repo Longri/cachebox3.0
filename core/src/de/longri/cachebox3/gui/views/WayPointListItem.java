403d25e30500e5ea255e3eaebfe783b66d6b5ca2
Longri <www.longri.de@googlemail.com>
2018-01-31 11:05:43 +0100

    [Core] extract WaypointItem from WaypointListItem for reuse on Bubble

--- core/src/de/longri/cachebox3/gui/views/WayPointListItem.java
+++ core/src/de/longri/cachebox3/gui/views/WayPointListItem.java
@@ -15,15 +15,7 @@
  */
 package de.longri.cachebox3.gui.views;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Disposable;
-import com.kotcrab.vis.ui.VisUI;
-import com.kotcrab.vis.ui.widget.VisLabel;
-import com.kotcrab.vis.ui.widget.VisTable;
-import de.longri.cachebox3.CB;
-import de.longri.cachebox3.gui.skin.styles.WayPointListItemStyle;
 import de.longri.cachebox3.gui.views.listview.ListViewItem;
 import de.longri.cachebox3.sqlite.Database;
 import de.longri.cachebox3.types.AbstractWaypoint;
@@ -40,113 +32,37 @@ public class WayPointListItem extends ListViewItem implements Disposable {
         return listViewItem;
     }
 
+    private final WayPointItem wayPointItem;
 
-    private final WayPointListItemStyle style;
-    private final CacheTypes type;
-    private final CharSequence wayPointGcCode;
-    private boolean needsLayout = true;
-    final private Image arrowImage;
-    final private VisLabel distanceLabel, descriptionLabel, coordLabel, nameLabel;
-    private boolean distanceOrBearingChanged = true;
-
-    public WayPointListItem(int listIndex, CacheTypes type, CharSequence wayPointGcCode, CharSequence wayPointTitle,
-                            CharSequence description, CharSequence coord) {
+    private WayPointListItem(int listIndex, CacheTypes type, CharSequence wayPointGcCode, CharSequence wayPointTitle,
+                             CharSequence description, CharSequence coord) {
         super(listIndex);
-        this.style = VisUI.getSkin().get("WayPointListItems", WayPointListItemStyle.class);
-        this.type = type;
-        this.wayPointGcCode = wayPointGcCode;
-
-        Label.LabelStyle nameLabelStyle = new Label.LabelStyle();
-        nameLabelStyle.font = this.style.nameFont;
-        nameLabelStyle.fontColor = this.style.nameFontColor;
-
-        nameLabel = new VisLabel(wayPointGcCode + ": " + wayPointTitle, nameLabelStyle);
-        descriptionLabel = description == null ? null : new VisLabel(description, nameLabelStyle);
-        coordLabel = new VisLabel(coord, nameLabelStyle);
-
-        nameLabel.setWrap(true);
-        descriptionLabel.setWrap(true);
-        coordLabel.setWrap(true);
-
-        arrowImage = new Image(this.style.arrow);
-
-        Label.LabelStyle distanceLabelStyle = new Label.LabelStyle();
-        distanceLabelStyle.font = this.style.distanceFont;
-        distanceLabelStyle.fontColor = this.style.distanceFontColor;
-
-        distanceLabel = new VisLabel("---- --", distanceLabelStyle);
+        this.wayPointItem = new WayPointItem(type, wayPointGcCode, wayPointTitle, description, coord);
+        this.add(wayPointItem).expand().fill();
     }
 
-
-    public synchronized void layout() {
-//        this.setDebug(true, false);
-        if (!needsLayout) {
-            super.layout();
-            return;
-        }
-
-        this.clear();
-
-        VisTable iconTable = new VisTable();
-        iconTable.add(type.getCacheWidget(style.typeStyle, null, null));
-
-        iconTable.pack();
-        iconTable.layout();
-
-        this.add(iconTable).left().top().padRight(CB.scaledSizes.MARGINx4);
-
-        Table contentTable = new Table();
-        contentTable.add(nameLabel).left().expandX().fillX();
-        contentTable.row();
-        contentTable.add(descriptionLabel).left().expandX().fillX();
-        contentTable.row();
-        contentTable.add(coordLabel).left().expandX().fillX();
-
-        this.add(contentTable).top().expandX().fillX();
-
-        VisTable arrowTable = new VisTable();
-
-
-        arrowImage.setOrigin(this.style.arrow.getMinWidth() / 2, this.style.arrow.getMinHeight() / 2);
-
-
-        arrowTable.add(arrowImage);
-        arrowTable.row();
-        arrowTable.add(distanceLabel).padTop(CB.scaledSizes.MARGIN);
-        this.add(arrowTable).right();
-        super.layout();
-        needsLayout = false;
-    }
 
     public boolean update(float bearing, CharSequence distance) {
-        if (!distanceOrBearingChanged) return false;
-        arrowImage.setRotation(bearing);
-        distanceLabel.setText(distance);
+        if (!this.wayPointItem.distanceOrBearingChanged) return false;
+        this.wayPointItem.arrowImage.setRotation(bearing);
+        this.wayPointItem.distanceLabel.setText(distance);
 
-        arrowImage.layout();
-        distanceLabel.layout();
-        distanceOrBearingChanged = false;
+        this.wayPointItem.arrowImage.layout();
+        this.wayPointItem.distanceLabel.layout();
+        this.wayPointItem.distanceOrBearingChanged = false;
         return true;
     }
 
     public void posOrBearingChanged() {
-        distanceOrBearingChanged = true;
+        this.wayPointItem.distanceOrBearingChanged = true;
     }
 
     @Override
     public synchronized void dispose() {
-        if (arrowImage != null) {
-            arrowImage.setDrawable(null);
-            arrowImage.clear();
-        }
-
-        if (distanceLabel != null) {
-            distanceLabel.setText(null);
-            distanceLabel.clear();
-        }
+        this.wayPointItem.dispose();
     }
 
     public CharSequence getWaypointGcCode() {
-        return this.wayPointGcCode;
+        return this.wayPointItem.wayPointGcCode;
     }
 }
