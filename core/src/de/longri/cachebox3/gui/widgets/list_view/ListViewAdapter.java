18200610601a6546b81046a61c1ce744d9e3b992
Longri <develop@longri.de>
2018-02-03 14:40:46 +0100

    [Core] create new empty classes for ListView

---
+++ core/src/de/longri/cachebox3/gui/widgets/list_view/ListViewAdapter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.gui.widgets.list_view;
+
+/**
+ * Created by Longri on 03.02.18.
+ */
+public interface ListViewAdapter {
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    int getCount();
+
+    /**
+     * get a View that displays the data at the specified position in the data set.
+     *
+     * @param index The position of the item within the adapter's data set of the item whose view we want.
+     * @return A View corresponding to the data at the specified position.
+     */
+    ListViewItem getView(int index);
+
+    /**
+     * @param view the ListViewItem for update
+     */
+    void update(ListViewItem view);
+
+    /**
+     * Returns the size for calculating the position of an item.</br> </br>
+     * For V_ListView => the height </br>
+     * For H_ListView => the width
+     *
+     * @param index The position of the item within the adapter's data set of the item whose view we want.
+     * @return Returns the size for calculating the position of an item
+     */
+    float getItemSize(int index);
+}
---
+++ core/src/de/longri/cachebox3/gui/widgets/list_view/ListViewItem.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2018 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.gui.widgets.list_view;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.utils.Disposable;
+import com.kotcrab.vis.ui.widget.VisTable;
+import de.longri.cachebox3.CB;
+
+/**
+ * Created by Longri on 03.02.18.
+ */
+public class ListViewItem extends VisTable implements Disposable {
+
+    private OnDrawListener onDrawListener;
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        if (getOnDrawListener() != null) {
+            if (CB.viewmanager != null && CB.viewmanager.isTop(this.getStage()))
+                getOnDrawListener().onDraw(this);
+        }
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public OnDrawListener getOnDrawListener() {
+        return onDrawListener;
+    }
+
+    public void setOnDrawListener(OnDrawListener onDrawListener) {
+        this.onDrawListener = onDrawListener;
+    }
+}
