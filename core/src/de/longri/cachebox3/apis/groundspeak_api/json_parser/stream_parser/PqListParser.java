1a49a6109f8d01ea34fb4c42195285465e55bc2f
Longri <www.longri.de@googlemail.com>
2018-03-26 15:12:42 +0200

    [Core] finish PQ List parser with JUnit

--- core/src/de/longri/cachebox3/apis/groundspeak_api/json_parser/stream_parser/PqListParser.java
+++ core/src/de/longri/cachebox3/apis/groundspeak_api/json_parser/stream_parser/PqListParser.java
@@ -19,27 +19,139 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.JsonStreamParser;
 import de.longri.cachebox3.apis.groundspeak_api.ApiResultState;
 import de.longri.cachebox3.apis.groundspeak_api.PocketQuery;
+import de.longri.cachebox3.utils.ICancel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Created by Longri on 26.03.2018.
  */
 public class PqListParser {
 
-    public ApiResultState parsePqList(InputStream stream, Array<PocketQuery.PQ> pqList) {
-        ApiResultState resultState = ApiResultState.IO;
+    private final Logger log = LoggerFactory.getLogger(PqListParser.class);
+
+    private final ICancel iCancel;
+
+    public PqListParser(ICancel icancel) {
+        this.iCancel = icancel;
+    }
+
+
+    private boolean pocketQueryList = false;
+
+    public ApiResultState parsePqList(InputStream stream, final Array<PocketQuery.PQ> pqList) {
+        final ApiResultState[] resultState = {ApiResultState.IO};
 
         // Parse JSON Result
         final JsonStreamParser parser = new JsonStreamParser() {
+
+            public String name;
+            public String guid;
+            public int cacheCount;
+            public Date lastGenerated;
+            public double sizeMB;
+            public boolean downloadAvailable = false;
+
+
+            @Override
+            public void startArray(String name) {
+                if (name.equals("PocketQueryList")) {
+                    pocketQueryList = true;
+                }
+            }
+
+            @Override
+            public void endArray(String name) {
+                if (name.equals("PocketQueryList")) {
+                    this.cancel();
+                }
+            }
+
+            @Override
+            public void pop() {
+                if (pocketQueryList) {
+                    // new PQ
+                    if (name != null) {
+                        PocketQuery.PQ newPQ = new PocketQuery.PQ();
+                        newPQ.name = name;
+                        newPQ.guid = guid;
+                        newPQ.cacheCount = cacheCount;
+                        newPQ.lastGenerated = lastGenerated;
+                        newPQ.sizeMB = sizeMB;
+                        newPQ.downloadAvailable = downloadAvailable;
+                        pqList.add(newPQ);
+                    }
+                    name = null;
+                }
+            }
+
+
+            @Override
+            public void string(String name, String value) {
+                if (iCancel != null && iCancel.cancel()) this.cancel();
+                super.string(name, value);
+
+                if (value.equals("null"))
+                    value = null;
+
+                if (pocketQueryList) {
+                    if (name.equals("GUID")) {
+                        this.guid = value;
+                    } else if (name.equals("Name")) {
+                        this.name = value;
+                    } else if (name.equals("DateLastGenerated")) {
+                        this.lastGenerated = getDateFromLongString(value);
+                    }
+                } else if (name.equals("StatusMessage") && value.contains("expired")) {
+                    // break reading and set ApiResultState
+                    resultState[0] = ApiResultState.EXPIRED_API_KEY;
+                    this.cancel();
+                }
+            }
+
+            @Override
             public void number(String name, long value, String stringValue) {
                 super.number(name, value, stringValue);
+                if (pocketQueryList) {
+                    if (name.equals("FileSizeInBytes")) {
+                        this.sizeMB = value;
+                    } else if (name.equals("PQCount")) {
+                        this.cacheCount = (int) value;
+                    }
+                }
+            }
 
+            @Override
+            public void bool(String name, boolean value) {
+                if (pocketQueryList) {
+                    if (name.equals("IsDownloadAvailable")) {
+                        this.downloadAvailable = value;
+                    }
+                }
             }
         };
         parser.parse(stream);
 
-        return resultState;
+        return resultState[0];
     }
 
+
+    private static final String DATE_START = "Date(";
+
+    private synchronized Date getDateFromLongString(String value) {
+        Date date = new Date();
+        try {
+            int date1 = value.indexOf(DATE_START);
+            int date2 = value.lastIndexOf("-");
+            String dateString = value.substring(date1 + DATE_START.length(), date2);
+            if (dateString.startsWith("\"")) dateString = dateString.substring(1);
+            date = new Date(Long.valueOf(dateString));
+        } catch (Exception exc) {
+            log.error("ParseDate from value:'{}'", value, exc);
+        }
+        return date;
+    }
 }
--- core/tests/de/longri/cachebox3/apis/groundspeak_api/json_parser/stream_parser/PqListParserTest.java
+++ core/tests/de/longri/cachebox3/apis/groundspeak_api/json_parser/stream_parser/PqListParserTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -19,13 +20,42 @@ class PqListParserTest {
     @Test
     void parsePqList() throws FileNotFoundException {
         InputStream stream = TestUtils.getResourceRequestStream("testsResources/GetPqListResult.json");
-        PqListParser parser = new PqListParser();
+        PqListParser parser = new PqListParser(null);
 
         Array<PocketQuery.PQ> pqList = new Array<>();
 
         ApiResultState result = parser.parsePqList(stream, pqList);
 
         assertThat("Result must be ApiResultState.IO", result == ApiResultState.IO);
-        assertThat("PQ List count must be 10", pqList.size == 10);
+        assertThat("PQ List count must be 6", pqList.size == 6);
+
+        for (int i = 0; i < pqList.size; i++) {
+            PocketQuery.PQ pq = pqList.get(i);
+            switch (i) {
+                case 0:
+                    assertThat("wrong value", pq.guid.equals("a1244aab-f585-4648-8dea-f9b979b533be"));
+                    assertThat("wrong value", pq.name.equals("Birkenwerder"));
+                    assertThat("wrong value", pq.lastGenerated.equals(new Date(1521969736000L)));
+                    assertThat("wrong value", pq.downloadAvailable == false);
+                    assertThat("wrong value", pq.sizeMB == 1899174);
+                    assertThat("wrong value", pq.cacheCount == 1000);
+                    break;
+                case 1:
+                    assertThat("wrong value", pq.guid.equals("85c3888c-d097-4055-b02e-0f182d71fa9e"));
+                    assertThat("wrong value", pq.name.equals("H&#246;now"));
+                    assertThat("wrong value", pq.lastGenerated.equals(new Date(1521971482000L)));
+                    assertThat("wrong value", pq.downloadAvailable == true);
+                    assertThat("wrong value", pq.sizeMB == 1753293);
+                    assertThat("wrong value", pq.cacheCount == 1000);
+                    break;
+                case 5:
+                    assertThat("wrong value", pq.guid.equals("e6071d91-9425-4828-94f2-543c7fafc899"));
+                    assertThat("wrong value", pq.name.equals("Wandlitz"));
+                    assertThat("wrong value", pq.lastGenerated.equals(new Date(1522049839000L)));
+                    assertThat("wrong value", pq.downloadAvailable == true);
+                    assertThat("wrong value", pq.sizeMB == 1847508);
+                    assertThat("wrong value", pq.cacheCount == 1000);
+            }
+        }
     }
 }
