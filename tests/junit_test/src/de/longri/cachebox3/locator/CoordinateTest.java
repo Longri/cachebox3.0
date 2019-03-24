90ff40fdd8f231d42eeda340d2eef04d0023ab05
Longri <develop@longri.de>
2019-03-24 10:54:05 +0100

    add UNitTest for LatLong and Coordinate to fix #212

---
+++ tests/junit_test/src/de/longri/cachebox3/locator/CoordinateTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2017 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.locator;
+
+import de.longri.cachebox3.utils.MathUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Longri on 25.10.2017.
+ */
+class CoordinateTest {
+
+    @Test
+    void project() {
+
+        int distance = 24;//meter
+
+        Coordinate c1 = new Coordinate(1, 1);
+        Coordinate c2 = Coordinate.Project(c1, 12, distance);
+        int calculatedDistance = Math.round(c1.distance(c2, MathUtils.CalculationType.FAST));
+
+        assertEquals(distance, calculatedDistance, "Distance must equals");
+
+    }
+
+    @Test
+    void stringConstructor() {
+        String str = "N 48° 40.441 E 009° 23.470";
+        Coordinate coord = new Coordinate(str);
+        assertEquals(48.674017, coord.getLatitude());
+        assertEquals(9.391167, coord.getLongitude());
+
+        str = "52°34′49.7316″N 13°23′8.3364″E";
+        coord = new Coordinate(str);
+        assertEquals(52.580481, coord.getLatitude());
+        assertEquals(13.385649, coord.getLongitude());
+
+    }
+
+    @Test
+    void equalsTest() {
+        Coordinate corrd1 = new Coordinate("N 52° 36.307 E 013° 21.517");
+        Coordinate corrd2 = new Coordinate("N 52° 36.435 E 013° 21.821");
+        Coordinate corrd3 = new Coordinate("N 52° 36.045 E 013° 21.760");
+
+        Coordinate corrd4 = new Coordinate("N 52° 36.307 E 013° 21.517");
+        Coordinate corrd5 = new Coordinate("N 52° 36.435 E 013° 21.821");
+        Coordinate corrd6 = new Coordinate("N 52° 36.045 E 013° 21.760");
+
+        Coordinate corrd7 = new Coordinate("N 52° 36.045000005 E 013° 21.760000002");
+
+
+        assertTrue(corrd1.equals(corrd1), "Coordinates.equals() must be true");
+        assertTrue(corrd2.equals(corrd2), "Coordinates.equals() must be true");
+        assertTrue(corrd3.equals(corrd3), "Coordinates.equals() must be true");
+
+        assertTrue(corrd4.equals(corrd4), "Coordinates.equals() must be true");
+        assertTrue(corrd5.equals(corrd5), "Coordinates.equals() must be true");
+        assertTrue(corrd6.equals(corrd6), "Coordinates.equals() must be true");
+
+        assertTrue(corrd1.equals(corrd4), "Coordinates.equals() must be true");
+        assertTrue(corrd2.equals(corrd5), "Coordinates.equals() must be true");
+        assertTrue(corrd3.equals(corrd6), "Coordinates.equals() must be true");
+
+        assertTrue(corrd7.equals(corrd6), "Coordinates.equals() must be true");
+        assertTrue(corrd6.equals(corrd7), "Coordinates.equals() must be true");
+
+        assertFalse(corrd2.equals(corrd1), "Coordinates.equals() must be false");
+        assertFalse(corrd1.equals(corrd2), "Coordinates.equals() must be false");
+
+        assertFalse(corrd3.equals(corrd1), "Coordinates.equals() must be false");
+        assertFalse(corrd1.equals(corrd3), "Coordinates.equals() must be false");
+
+        assertFalse(corrd2.equals(corrd3), "Coordinates.equals() must be false");
+        assertFalse(corrd3.equals(corrd2), "Coordinates.equals() must be false");
+
+    }
+
+}
---
+++ tests/junit_test/src/de/longri/cachebox3/locator/LatLongTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2019 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.cachebox3.locator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by Longri on 2019-03-24.
+ */
+public class LatLongTest {
+
+    @Test
+    void equalsTest() {
+
+        LatLong latLong1 = new LatLong(52.605117, 13.358617);
+        LatLong latLong2 = new LatLong(52.60725, 13.363683);
+        LatLong latLong3 = new LatLong(52.60075, 13.362667);
+
+        LatLong latLong4 = new LatLong(52.605117, 13.358617);
+        LatLong latLong5 = new LatLong(52.60725, 13.363683);
+        LatLong latLong6 = new LatLong(52.60075, 13.362667);
+
+        LatLong latLong7 = new LatLong(52.6007500001, 13.362667000002);
+
+        assertTrue(latLong1.equals(latLong1), "Coordinates.equals() must be true");
+        assertTrue(latLong2.equals(latLong2), "Coordinates.equals() must be true");
+        assertTrue(latLong3.equals(latLong3), "Coordinates.equals() must be true");
+
+        assertTrue(latLong4.equals(latLong4), "Coordinates.equals() must be true");
+        assertTrue(latLong5.equals(latLong5), "Coordinates.equals() must be true");
+        assertTrue(latLong6.equals(latLong6), "Coordinates.equals() must be true");
+
+        assertTrue(latLong1.equals(latLong4), "Coordinates.equals() must be true");
+        assertTrue(latLong2.equals(latLong5), "Coordinates.equals() must be true");
+        assertTrue(latLong3.equals(latLong6), "Coordinates.equals() must be true");
+
+        assertFalse(latLong7.equals(latLong6), "Coordinates.equals() must be false");
+        assertFalse(latLong6.equals(latLong7), "Coordinates.equals() must be false");
+
+        assertFalse(latLong2.equals(latLong1), "Coordinates.equals() must be false");
+        assertFalse(latLong1.equals(latLong2), "Coordinates.equals() must be false");
+
+        assertFalse(latLong3.equals(latLong1), "Coordinates.equals() must be false");
+        assertFalse(latLong1.equals(latLong3), "Coordinates.equals() must be false");
+
+        assertFalse(latLong2.equals(latLong3), "Coordinates.equals() must be false");
+        assertFalse(latLong3.equals(latLong2), "Coordinates.equals() must be false");
+
+    }
+}
