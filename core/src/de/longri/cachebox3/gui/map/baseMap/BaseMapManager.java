Longri/cachebox3.0

commit 7aa33a35da1b864529a81d4006545cfd7504e769

fix JUnit test are broken with commit fbf5bbb617b51e7cd8777e2d4d9cc64b1cc9666b

core/src/de/longri/cachebox3/gui/map/baseMap/BaseMapManager.java
@@ -22,7 +22,6 @@ import de.longri.cachebox3.Utils;
 import de.longri.cachebox3.settings.Config;
 import de.longri.cachebox3.utils.lists.CB_List;
 
-import java.io.File;
 import java.io.FileFilter;
 
 /**
@@ -32,12 +31,15 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
 
     public final static BaseMapManager INSTANCE = new BaseMapManager();
 
-    public BaseMapManager() {
+    private BaseMapManager() {
     }
 
     public void refreshMaps() {
+        refreshMaps(Gdx.files.absolute(CB.WorkPath));
+    }
+
+    private void refreshMaps(FileHandle workPath) {
         this.clear();
-        FileHandle workPath = Gdx.files.absolute(CB.WorkPath);
         // add map files from repository
         String repositoryMaps = addMapFiles(workPath.child("repository").child("maps"));
 
@@ -55,12 +57,7 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
     }
 
     private String addMapFiles(FileHandle workPath) {
-        FileFilter mapFileFilter = new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return Utils.getFileExtension(pathname.getAbsolutePath()).toLowerCase().equals("map");
-            }
-        };
+        FileFilter mapFileFilter = pathname -> Utils.getFileExtension(pathname.getAbsolutePath()).toLowerCase().equals("map");
         FileHandle[] mapFiles = workPath.list(mapFileFilter);
 
         for (FileHandle mapFile : mapFiles) {

commit cb3e06aef6227941fd5db4e45dda7686e87c7c03

fix JUnit test are broken with commit 7aa33a35da1b864529a81d4006545cfd7504e769

core/src/de/longri/cachebox3/gui/map/baseMap/BaseMapManager.java
@@ -31,14 +31,14 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
 
     public final static BaseMapManager INSTANCE = new BaseMapManager();
 
-    private BaseMapManager() {
+    public BaseMapManager() {
     }
 
     public void refreshMaps() {
         refreshMaps(Gdx.files.absolute(CB.WorkPath));
     }
 
-    private void refreshMaps(FileHandle workPath) {
+    public void refreshMaps(FileHandle workPath) {
         this.clear();
         // add map files from repository
         String repositoryMaps = addMapFiles(workPath.child("repository").child("maps"));