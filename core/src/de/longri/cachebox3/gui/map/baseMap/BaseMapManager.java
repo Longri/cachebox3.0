Longri/cachebox3.0

commit fbf5bbb617b51e7cd8777e2d4d9cc64b1cc9666b

* add getBaseMap from cacheboxMapadapter (need for MapView ContextMenu) * add todo for preload theme * correct refreshMaps List: no double add , removed param (avoid setting wrong workpath)

core/src/de/longri/cachebox3/gui/map/baseMap/BaseMapManager.java
@@ -17,6 +17,7 @@ package de.longri.cachebox3.gui.map.baseMap;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import de.longri.cachebox3.CB;
 import de.longri.cachebox3.Utils;
 import de.longri.cachebox3.settings.Config;
 import de.longri.cachebox3.utils.lists.CB_List;
@@ -34,25 +35,26 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
     public BaseMapManager() {
     }
 
-    public void refreshMaps(FileHandle workPath) {
+    public void refreshMaps() {
         this.clear();
-
+        FileHandle workPath = Gdx.files.absolute(CB.WorkPath);
         // add map files from repository
-        addMapFiles(workPath.child("repository").child("maps"));
+        String repositoryMaps = addMapFiles(workPath.child("repository").child("maps"));
 
         // add map files from act selected repository
         String actRepositoryName = Config.DatabaseName.getValue().replace(".db3", "");
         addMapFiles(workPath.child("repositories").child(actRepositoryName).child("maps"));
 
         // add map files from setet user map folder
-        String userMapFolder = Config.MapPackFolder.getValue();
-        addMapFiles(Gdx.files.absolute(userMapFolder));
+        FileHandle userMapFolder = Gdx.files.absolute(Config.MapPackFolder.getValue());
+        if (!userMapFolder.path().equals(repositoryMaps))
+            addMapFiles(userMapFolder);
 
         this.addStaticMaps();
         this.sort();
     }
 
-    private void addMapFiles(FileHandle workPath) {
+    private String addMapFiles(FileHandle workPath) {
         FileFilter mapFileFilter = new FileFilter() {
             @Override
             public boolean accept(File pathname) {
@@ -64,6 +66,7 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
         for (FileHandle mapFile : mapFiles) {
             this.add(new MapsforgeSingleMap(mapFile));
         }
+        return workPath.path();
     }
 
     private void addStaticMaps() {

core/src/de/longri/cachebox3/gui/CacheboxMapAdapter.java
@@ -57,7 +57,7 @@ public class CacheboxMapAdapter extends Map implements Map.UpdateListener {
     private boolean mRenderRequest;
     private int width = Gdx.graphics.getWidth(), height = Gdx.graphics.getHeight(), xOffset, yOffset;
     private VectorTileLayer vectorTileLayer;
-
+    private AbstractManagedMapLayer baseMap;
 
     public CacheboxMapAdapter() {
         super();
@@ -117,7 +117,6 @@ public class CacheboxMapAdapter extends Map implements Map.UpdateListener {
             }
         }
     }
-
 
     @Override
     public void render() {
@@ -187,7 +186,12 @@ public class CacheboxMapAdapter extends Map implements Map.UpdateListener {
         return super.handleGesture(g, e);
     }
 
+    public AbstractManagedMapLayer getBaseMap() {
+        return baseMap;
+    }
+
     public void setNewBaseMap(final AbstractManagedMapLayer baseMap) {
+        this.baseMap = baseMap;
         if (this.layers().size() > 1)
             this.layers().remove(1);
 

core/src/de/longri/cachebox3/gui/stages/initial_tasks/SkinLoaderTask.java
@@ -172,7 +172,7 @@ public final class SkinLoaderTask extends AbstractInitTask {
             storeAttributePng(skin, style, attFileHandle, value);
         }
 
-
+        // todo do not preload theme, if no mapsforge map is selected
         //preload Map Theme on async task
         CB.postAsync(new NamedRunnable("preload Map Theme") {
             @Override

core/src/de/longri/cachebox3/gui/views/MapView.java
@@ -598,7 +598,7 @@ public class MapView extends AbstractView {
 
         // load last saved BaseMap
         String baseMapName = Settings_Map.CurrentMapLayer.getValue()[0];
-        BaseMapManager.INSTANCE.refreshMaps(Gdx.files.absolute(Settings_Map.MapPackFolder.getValue()));
+        BaseMapManager.INSTANCE.refreshMaps();
         AbstractManagedMapLayer baseMap = null;
         for (int i = 0, n = BaseMapManager.INSTANCE.size; i < n; i++) {
             AbstractManagedMapLayer map = BaseMapManager.INSTANCE.get(i);
@@ -731,21 +731,23 @@ public class MapView extends AbstractView {
     @Override
     public Menu getContextMenu() {
         Menu icm = new Menu("MapViewContextMenuTitle");
-        icm.addMenuItem("Layer", CB.getSkin().getMenuIcon.mapLayer, () -> showMapLayerMenu());
-        icm.addMenuItem("Renderthemes", CB.getSkin().getMenuIcon.theme, () -> showMapViewThemeMenu());
-        icm.addMenuItem("Styles", CB.getSkin().getMenuIcon.themeStyle, () -> showMapViewThemeStyleMenu());
-        icm.addMenuItem("overlays", null, () -> showMapOverlayMenu()); // todo icon
-        icm.addMenuItem("view", CB.getSkin().getMenuIcon.viewSettings, () -> showMapViewLayerMenu());
+        icm.addMenuItem("Layer", CB.getSkin().getMenuIcon.mapLayer, () -> showMapViewLayerMenu());
+        if (cacheboxMapAdapter.getBaseMap() instanceof MapsforgeSingleMap) {
+            icm.addMenuItem("Renderthemes", CB.getSkin().getMenuIcon.theme, () -> showMapViewThemeMenu());
+            icm.addMenuItem("Styles", CB.getSkin().getMenuIcon.themeStyle, () -> showMapViewThemeStyleMenu());
+        }
+        icm.addMenuItem("overlays", CB.getSkin().getMenuIcon.todo, () -> showMapViewOverlaysMenu()); // todo icon
+        icm.addMenuItem("view", CB.getSkin().getMenuIcon.viewSettings, () -> showMapViewElementsMenu());
         // todo needed? nach Kompass ausrichten | setAlignToCompass
         icm.addMenuItem("CenterWP", CB.getSkin().getMenuIcon.addWp, () -> createWaypointAtCenter());
-        icm.addMenuItem("RecTrack", null, () -> showMenuTrackRecording()); // todo icon
+        icm.addMenuItem("RecTrack", CB.getSkin().getMenuIcon.todo, () -> showTrackRecordMenu()); // todo icon
         return icm;
     }
 
-    private void showMapLayerMenu() {
+    private void showMapViewLayerMenu() {
         Menu icm = new Menu("MapViewLayerMenuTitle");
 
-        BaseMapManager.INSTANCE.refreshMaps(Gdx.files.absolute(CB.WorkPath));
+        BaseMapManager.INSTANCE.refreshMaps();
 
 
         int menuID = 0;
@@ -838,7 +840,7 @@ public class MapView extends AbstractView {
     }
 
     //todo ISSUE (#110 add MapView Overlays)
-    private void showMapOverlayMenu() {
+    private void showMapViewOverlaysMenu() {
         final Menu icm = new Menu("MapViewOverlayMenuTitle");
 
 //        int menuID = 0;
@@ -861,7 +863,7 @@ public class MapView extends AbstractView {
 //                }
 //                // Refresh menu
 //                icm.close();
-//                showMapOverlayMenu();
+//                showMapViewOverlaysMenu();
 //                return true;
 //            }
 //        });
@@ -869,7 +871,7 @@ public class MapView extends AbstractView {
         icm.show();
     }
 
-    private void showMapViewLayerMenu() {
+    private void showMapViewElementsMenu() {
         Menu icm = new Menu("MapViewElementsMenuTitle");
         icm.addCheckableMenuItem("HideFinds", Settings_Map.MapHideMyFinds.getValue(), () -> toggleSettingWithReload(Settings_Map.MapHideMyFinds));
         // icm.addCheckableMenuItem("MapShowCompass", Settings_Map.MapShowCompass.getValue(),()-> toggleSetting(Settings_Map.MapShowCompass));
@@ -1106,7 +1108,7 @@ public class MapView extends AbstractView {
     }
 
     //todo ISSUE (#112 Record Track)
-    private void showMenuTrackRecording() {
+    private void showTrackRecordMenu() {
         Menu cm2 = new Menu("TrackRecordMenuTitle");
         cm2.addMenuItem("start", null, () -> TrackRecorder.INSTANCE.startRecording()).setEnabled(!TrackRecorder.INSTANCE.recording);
         if (TrackRecorder.INSTANCE.pauseRecording)

commit 7aa33a35da1b864529a81d4006545cfd7504e769

fix JUnit test are broken with commit fbf5bbb617b51e7cd8777e2d4d9cc64b1cc9666b

core/src/de/longri/cachebox3/gui/map/baseMap/BaseMapManager.java
@@ -22,7 +22,6 @@ import de.longri.cachebox3.Utils;
 import de.longri.cachebox3.settings.Config;
 import de.longri.cachebox3.utils.lists.CB_List;
 
-import java.io.File;
 import java.io.FileFilter;
 
 /**
@@ -32,12 +31,15 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
 
     public final static BaseMapManager INSTANCE = new BaseMapManager();
 
-    public BaseMapManager() {
+    private BaseMapManager() {
     }
 
     public void refreshMaps() {
+        refreshMaps(Gdx.files.absolute(CB.WorkPath));
+    }
+
+    private void refreshMaps(FileHandle workPath) {
         this.clear();
-        FileHandle workPath = Gdx.files.absolute(CB.WorkPath);
         // add map files from repository
         String repositoryMaps = addMapFiles(workPath.child("repository").child("maps"));
 
@@ -55,12 +57,7 @@ public final class BaseMapManager extends CB_List<AbstractManagedMapLayer> {
     }
 
     private String addMapFiles(FileHandle workPath) {
-        FileFilter mapFileFilter = new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return Utils.getFileExtension(pathname.getAbsolutePath()).toLowerCase().equals("map");
-            }
-        };
+        FileFilter mapFileFilter = pathname -> Utils.getFileExtension(pathname.getAbsolutePath()).toLowerCase().equals("map");
         FileHandle[] mapFiles = workPath.list(mapFileFilter);
 
         for (FileHandle mapFile : mapFiles) {