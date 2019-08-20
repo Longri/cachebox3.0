e9f93c4cbc956cca6cc2f60b359a0651a68c2239
Longri <www.longri.de@googlemail.com>
2019-08-20 15:59:16 +0200

    add test for exist all style classes at package de.longri.cachebox3.gui.skin.styles

--- tests/junit_test/src/de/longri/cachebox3/gui/SkinTest.java
+++ tests/junit_test/src/de/longri/cachebox3/gui/SkinTest.java
@@ -18,13 +18,29 @@ package de.longri.cachebox3.gui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.ui.SvgSkin;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 import de.longri.cachebox3.CB;
 import de.longri.cachebox3.TestUtils;
+import de.longri.cachebox3.gui.skin.styles.AbstractIconStyle;
+import de.longri.cachebox3.gui.skin.styles.EditWaypointStyle;
 import de.longri.cachebox3.platform_test.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by Longri on 20.08.2019.
@@ -70,6 +86,21 @@ public class SkinTest {
     @Test
     void loadSkinJsonTest() {
         assertNotNull(testSkin, "Skin must be loaded and not NULL");
+    }
 
+    @Test
+    void existAllStyleClasses() throws ClassNotFoundException {
+        List<Class<?>> clazzList = TestUtils.getClassesInSamePackage(AbstractIconStyle.class);
+        for (Class<?> clazz : clazzList) {
+
+            if (clazz.equals(EditWaypointStyle.class)) {
+                System.out.println("Must implement the missing EditWaypointStyle");
+                continue; // TODO implement EditWaypointStyle
+            }
+
+            ObjectMap<String, ?> allStyles = testSkin.getAll(clazz);
+            assertNotNull(allStyles, "Style for class:" + clazz.toString() + " must not be Null");
+            assertTrue(allStyles.size > 0);
+        }
     }
 }
