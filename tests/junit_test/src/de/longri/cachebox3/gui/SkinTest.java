e0d3bf0cdbea2afc04f0b0da5d32bb91ebd855c0
Longri <www.longri.de@googlemail.com>
2019-08-21 12:29:09 +0200

    improve skin test: don't copy Skin, test ==> android/assets/skin/day direkt

--- tests/junit_test/src/de/longri/cachebox3/gui/SkinTest.java
+++ tests/junit_test/src/de/longri/cachebox3/gui/SkinTest.java
@@ -18,7 +18,6 @@ package de.longri.cachebox3.gui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.ui.SvgSkin;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
 import de.longri.cachebox3.CB;
 import de.longri.cachebox3.TestUtils;
@@ -28,19 +27,10 @@ import de.longri.cachebox3.platform_test.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by Longri on 20.08.2019.
@@ -53,26 +43,12 @@ public class SkinTest {
     @BeforeAll
     static void initial() {
         TestUtils.initialGdx();
-
-        ///we want to test the current skin,
-        // so we have to copy it to the Resources first!
-
-        FileHandle androidAssetSkin = Gdx.files.absolute("../../launcher/android/assets/skins");
-        FileHandle resourceFileHandle = TestUtils.getResourceFileHandle("testsResources", true);
-
-        // delete exist skin for clean testing
-        resourceFileHandle.child("skins").deleteDirectory();
-        androidAssetSkin.copyTo(resourceFileHandle);
-
-
-        //Load the Skin for all tests
+        final FileHandle androidAssetSkin = Gdx.files.absolute("../../launcher/android/assets/skins/day");
         final String finalSkinName = "internalDefault";
         final SvgSkin.StorageType finalType = SvgSkin.StorageType.INTERNAL;
-        final FileHandle finalSkinFileHandle = TestUtils.getResourceFileHandle("testsResources", true).child("skins/day");
-        testSkin = new SvgSkin(true, finalSkinName, finalType, finalSkinFileHandle);
+        testSkin = new SvgSkin(true, finalSkinName, finalType, androidAssetSkin);
         CB.setActSkin(testSkin);
         CB.backgroundColor = CB.getColor("background");
-
     }
 
     @AfterAll
@@ -92,7 +68,10 @@ public class SkinTest {
     void existAllStyleClasses() throws ClassNotFoundException {
         List<Class<?>> clazzList = TestUtils.getClassesInSamePackage(AbstractIconStyle.class);
         for (Class<?> clazz : clazzList) {
-
+            if (clazz.equals(AbstractIconStyle.class)) {
+                System.out.println("ignore AbstractIconStyle.class");
+                continue; // ignore abstract classes
+            }
             if (clazz.equals(EditWaypointStyle.class)) {
                 System.out.println("Must implement the missing EditWaypointStyle");
                 continue; // TODO implement EditWaypointStyle
